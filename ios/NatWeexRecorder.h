afc7501473387814beb58a19422de36cd1c502ea
黄亚克 <13323829533@163.com>
2017-02-16 13:34:37 +0800

    0.0.1

---
+++ ios/NatWeexRecorder.h
@@ -0,0 +1,23 @@
+//
+//  NatWeexRecorder.h
+//
+//  Created by huangyake on 17/1/7.
+//  Copyright © 2017 Nat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <WeexSDK/WeexSDK.h>
+
+
+@protocol NatRecorderPro <WXModuleProtocol>
+
+- (void)start:(NSDictionary *)options :(WXModuleCallback)callback;
+- (void)pause:(WXModuleCallback)callback;
+- (void)stop:(WXModuleCallback)callback;
+
+
+@end
+
+@interface NatWeexRecorder : NSObject<NatRecorderPro>
+@end
---
+++ ios/NatWeexRecorder.m
@@ -0,0 +1,71 @@
+//
+//  NatWeexRecorder.m
+//
+//  Created by huangyake on 17/1/7.
+//  Copyright © 2017 Nat. All rights reserved.
+//
+
+#import "NatWeexRecorder.h"
+#import "NatRecorder.h"
+#define kRecordAudioFile @"myRecord.caf"
+
+@implementation NatWeexRecorder
+WX_EXPORT_METHOD(@selector(start::))
+WX_EXPORT_METHOD(@selector(pause:))
+WX_EXPORT_METHOD(@selector(stop:))
+
+
+
+- (void)start:(NSDictionary *)options :(WXModuleCallback)callback{
+    
+    [[NatRecorder singletonManger] start:options :^(id error,id result) {
+        if (error) {
+            if (callback) {
+                callback(error);
+            }
+        }else{
+            if (callback) {
+                callback(result);
+            }
+        }
+
+    }];
+    
+}
+- (void)pause:(WXModuleCallback)callback{
+    [[NatRecorder singletonManger] pause:^(id error,id result) {
+        if (error) {
+            if (callback) {
+                callback(error);
+            }
+        }else{
+            if (callback) {
+                callback(result);
+            }
+        }
+
+    }];
+    
+}
+- (void)stop:(WXModuleCallback)callback{
+    [[NatRecorder singletonManger] stop:^(id error,id result) {
+        if (error) {
+            if (callback) {
+                callback(error);
+            }
+        }else{
+            if (callback) {
+                callback(result);
+            }
+        }
+
+    }];
+}
+
+
+- (void)dealloc{
+    [[NatRecorder singletonManger] close];
+}
+
+
+@end
