4211ebacf59f87ae675a1fbbd602ee05e9f10671
Acathur <chiahungzhang@gmail.com>
2017-08-18 19:14:50 +0800

    0.1.3 released

--- ios/WeexNatRecorder.m
+++ ios/WeexNatRecorder.m
@@ -2,7 +2,7 @@
 //  WeexNatRecorder.m
 //
 //  Created by huangyake on 17/1/7.
-//  Copyright © 2017 Nat. All rights reserved.
+//  Copyright © 2017 Instapp. All rights reserved.
 //
 
 #import "WeexNatRecorder.h"
@@ -10,6 +10,7 @@
 #define kRecordAudioFile @"myRecord.caf"
 
 @implementation WeexNatRecorder
+@synthesize weexInstance;
 WX_EXPORT_METHOD(@selector(start::))
 WX_EXPORT_METHOD(@selector(pause:))
 WX_EXPORT_METHOD(@selector(stop:))
